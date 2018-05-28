46b7f89cab6769cff37e43a929247c8105757482
pdowney <downeypa@gmail.com>
2018-05-28 22:42:11 +0100

    Changing a comment

--- src/main/java/com/kepler76/algo/GCD.java
+++ src/main/java/com/kepler76/algo/GCD.java
@@ -2,7 +2,7 @@ package com.kepler76.algo;
 
 
 public class GCD {
-    // Adding a comment
+    // Adding a comment 1
     public static int gcd(int x, int y) {
         if(y  == 0) return x;
         return gcd(y, x % y);
